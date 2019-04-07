dee2340fa5540867e7cc8584c0b1ac9e57f13fee
vincenz5 <vincenzbuhler@gmail.com>
2019-04-06 20:16:58 -0400

    added in database, changed photos, channged linked image links

---
+++ test/insert.php
@@ -0,0 +1,52 @@
+<?php
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+    
+    
+    // $conn = '';
+    // $query = '';
+    // $dbconn = '';
+
+
+    $host = "localhost"; // Host name 
+    $username = "vincenzb_v"; // Mysql username 
+    $password = "passwords"; // Mysql password 
+    $db_name = "vincenzb_1"; // Database name 
+    // Connect to server and select databse. 
+    // Create connection
+    $conn = new mysqli($host, $username, $password, $db_name);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        } 
+
+    $dbconn = mysqli_connect($host, $username, $password)or die("cannot connect");
+    mysqli_select_db($dbconn, $db_name) or die("cannot select DB"); 
+
+    // $venue= 's';
+    // $date='e';
+    // $time='x';
+    // $postcode='y';
+
+    $name = $conn->real_escape_string($_POST['name']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $subject = $conn->real_escape_string($_POST['subject']);
+    $message = $conn->real_escape_string($_POST['message']);
+
+    // mysqli_real_escape_string($venue, $date, $time, $postcode);
+
+    $query = "INSERT INTO intouch (`name`, `email`, `subject`, `message`) VALUES ('$name', '$email', '$subject', '$message')";
+
+    mysqli_query($conn, $query);
+
+ // This will print whatever the user entered into the index.html page.
+ $name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
+ echo "<p style='font-size:20px;margin-top:20px;margin-left:30px;'>Thank you for your submission,". $_POST['name'] ."! We will be in touch soon. </p> <br>";
+
+ echo 
+ "<div class='all-post text-center col-lg-12'>
+ <a class='btn btn-main' href='/test'>Return</a>
+</div>";
+
+
+?>
