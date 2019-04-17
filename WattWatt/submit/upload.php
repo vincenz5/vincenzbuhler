d9d970768158ad55b25dfa26ef2f90dc3d55589b
vincenz5 <vincenzbuhler@gmail.com>
2019-04-17 15:14:20 -0400

    some work on photo uploads

---
+++ WattWatt/submit/upload.php
@@ -0,0 +1,33 @@
+<?php
+$target_dir = "\/home\/vincenzb\/public_html\/submittal";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// if ($_FILES["fileToUpload"]["size"] > 50000000) {
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
+
+// // Allow certain file formats
+// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+// && $imageFileType != "gif" ) {
+//     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//     $uploadOk = 0;
+// }
+
+// Check if image file is a actual image or fake image
+if(isset($_GET["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+    if($uploadOk = 1) {
+        echo "<br>Submitted!";
+    }
+}
+?>
